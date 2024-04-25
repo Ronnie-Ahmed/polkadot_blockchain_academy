31d11fb3156f52fccb707adf54716ab20589a0b2
Ronnie-Ahmed <rksraisul@gmail.com>
2024-04-25 18:13:27 +0600

    Added Type

--- blockchain_from_scratch/src/c1_state_machine/mod.rs
+++ blockchain_from_scratch/src/c1_state_machine/mod.rs
@@ -2,5 +2,18 @@ mod p1_switches;
 
 
 pub trait StateMachine {
+    type State;
+    type Transition;
+    fn next_state(state:&Self::State,t:&Self::Transition)->Self::State;
+    fn human_name()->String{
+        "Unnamed state Machine".into()
+    }
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+pub enum User {
+    Alice,
+    Bob,
+    Charlie
     
 }
